cb79f1127089f43aeb7430ffb4cc972a1af3a133
Nolankicks <nolandimensions@gmail.com>
2024-04-23 11:35:49 -0400

    More demo work

--- code/ShopList.cs
+++ code/ShopList.cs
@@ -5,7 +5,7 @@ namespace Kicks;
 public class ShopItems
 {
 	public List<string> ItemNames { get; set; } = new();
-    public delegate void ShopActionDelegate( PlayerController PlayerController, Inventory Inventory );
+    public delegate void ShopActionDelegate( PlayerController PlayerController, Inventory Inventory, AmmoContainer ammoContainer );
 	public List<ShopActionDelegate> ShopActions { get; set; } = new();
 	public List<Texture> ItemTextures { get; set; } = new();
 	public List<int> ItemPrices { get; set; } = new();
